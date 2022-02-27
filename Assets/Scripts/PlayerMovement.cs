def30d4b438f154e22ab32d6029a3fc8505ea2d7
Eduardo Pèrez <eduardo.perez.2151@gmail.com>
2022-02-27 04:13:07 -0300

    feat: adding wall climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,71 +6,123 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed;
+
+    [SerializeField] private float jumpPower;
+
+    [SerializeField] private float climbPower;
+
+    [SerializeField] private LayerMask groundLayerMask;
+
+    [SerializeField] private LayerMask wallLayerMask;
+
+    private float wallJumpCoolDown;
+    private float horizontalInput;
 
     private Rigidbody2D body;
 
     private Animator animator;
 
-    private bool grounded;
+    private BoxCollider2D boxCollider;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.body = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
-        
+        this.boxCollider = GetComponent<BoxCollider2D>();
+
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
-        Debug.Log(collision.gameObject.tag);
-        if (collision.gameObject.tag == "Ground")
-        {
-            this.grounded = true;
-        }
-    }
-
-    
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        this.horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(this.horizontalInput * speed, body.velocity.y);
 
         //Flip player horizontal
-        if (horizontalInput > 0.01f)
+        if (this.horizontalInput > 0.01f)
         {
             transform.localScale = new Vector3(0.5f, 0.5f, 1);
         }
-        else if (horizontalInput < -0.01f)
+        else if (this.horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-.5f, .5f, 1);
         }
 
-        if (CanJump())
-        {
-            Jump();
-        }
 
         //set animation parameters
-        this.animator.SetBool("run", horizontalInput != 0);
-        this.animator.SetBool("grounded", this.grounded);
+        this.animator.SetBool("run", this.horizontalInput != 0);
+        this.animator.SetBool("grounded", this.IsGrounded());
+
+        //Wall jump logic
+        if (wallJumpCoolDown > 0.2f)
+        {
+
+
+            body.velocity = new Vector2(this.horizontalInput * speed, body.velocity.y);
+
+            if (OnWall() && !IsGrounded())
+            {
+                this.body.gravityScale = 0;
+                this.body.velocity = Vector2.zero;
+            }
+            else
+            {
+                this.body.gravityScale = 2.5f;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+
+        }
+        else
+        {
+            this.wallJumpCoolDown += Time.deltaTime;
+        }
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        this.animator.SetTrigger("jump");
-        this.grounded = false;
+        if (IsGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            this.animator.SetTrigger("jump");
+        }
+        else if (OnWall() && !IsGrounded())
+        {
+
+            if (horizontalInput == 0)
+            {
+
+                this.body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 20, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                this.body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * climbPower, 6);
+            }
+
+            this.wallJumpCoolDown = 0;
+
+        }
     }
 
-    private bool CanJump()
+    private bool IsGrounded()
     {
-        return Input.GetKey(KeyCode.Space) && this.grounded;
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(this.boxCollider.bounds.center, this.boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayerMask);
+        return raycastHit.collider != null;
+    }
+
+    private bool OnWall()
+    {
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(this.boxCollider.bounds.center, this.boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayerMask);
+        return raycastHit.collider != null;
     }
 
 }
